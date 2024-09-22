e72c2aff4d7c9fdb0de476c648ef382050b5a55e
Shanick <44624042+shanickcuello@users.noreply.github.com>
2024-09-22 22:00:50 +0200

    Update auto_install.sh with impacket settings

--- auto_install.sh
+++ auto_install.sh
@@ -30,18 +30,19 @@ smbmap \
 autokey-gtk \
 seclists
 
-# Pipx settings
-pipx ensurepath
-
 # Python installs
 python3 -m pipx install impacket
+
+# Pipx settings
+pipx ensurepath
 
 # Update nmap scripts
 sudo nmap --script-updatedb
 
 #   Manual steps
 # install fish and then run
-#chsh -s /usr/bin/fish
+# chsh -s /usr/bin/fish
+# Add to path fish_add_path -g /home/vidar/.local/bin/
 
 #execute crontab -e and add
 #00 2 ,15,30 * * autoremove /home/[USER NAME]/automations/apt_autoremove.sh
